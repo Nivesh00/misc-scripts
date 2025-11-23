613fb3a2bdacfcd7e29a0a2240c07053fb00cf20
Nivesh <niveshramlochun055@gmail.com>
2025-11-23 14:16:29 +0100

    Added functionality to delete from multiple pages

--- ghcr-clean-images/delete-untagged-images.py
+++ ghcr-clean-images/delete-untagged-images.py
@@ -10,29 +10,38 @@ GITHUB_TOKEN :str = os.getenv("GITHUB_TOKEN", "")
 
 def getImageIds(repo :str, token :str) -> list[int]:
     
-    try:
-        # Get all images
-        resp :requests.Response = requests.get(
-            url     = f"https://api.github.com/user/packages/container/{repo}/versions",
-            headers = {
-                "Authorization": f"Bearer {token}"
-            }
-        )
-    except requests.exceptions.RequestException as e:
-        raise SystemExit(e)        
-
-    # Convert response to json
-    respJson :list[dict[str, Any]] = resp.json()
-
     # Get all ids
     ids :list[int] = []
-    for imageData in respJson:
 
-        # Get tags for image
-        tags :list[str] = imageData["metadata"]["container"]["tags"]
-        # If tag list is empty, add to id list
-        if not len(tags):
-            ids.append(imageData["id"])
+    # Request page
+    page = 1
+    while True:
+        try:
+            # Get all images
+            resp :requests.Response = requests.get(
+                url     = f"https://api.github.com/user/packages/container/{repo}/versions?page={page}&per_page=100",
+                headers = {
+                    "Authorization": f"Bearer {token}"
+                },
+                timeout = 10
+            )
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+        # Convert response to json
+        respJson :list[dict[str, Any]] = resp.json()
+        if not respJson: break
+
+        for imageData in respJson:
+
+            # Get tags for image
+            tags :list[str] = imageData["metadata"]["container"]["tags"]
+            # If tag list is empty, add to id list
+            if not len(tags):
+                ids.append(imageData["id"])
+        
+        # Check next page
+        page += 1
 
     return ids
 
@@ -62,6 +71,7 @@ if __name__ == '__main__':
     # Print log
     idsStrings :list[str] = [str(id) for id in ids]
     print(f"Images with following ids will be deleted from repository '{REPOSITORY}': [{', '.join(idsStrings)}]")
+    print(len(idsStrings))
 
     # Delete all images from response above
     deleteUntaggedImages(REPOSITORY, GITHUB_TOKEN, ids)
