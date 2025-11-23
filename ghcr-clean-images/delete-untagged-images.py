646ad867e15ba53794d7c297013382468ea0f790
Nivesh <niveshramlochun055@gmail.com>
2025-11-23 10:21:50 +0100

    Created script

---
+++ ghcr-clean-images/delete-untagged-images.py
@@ -0,0 +1,72 @@
+import requests, os, dotenv
+from typing import Any
+
+
+# Load .dotenv file
+dotenv.load_dotenv()
+REPOSITORY   :str = os.getenv("REPOSITORY", "")
+GITHUB_TOKEN :str = os.getenv("GITHUB_TOKEN", "")
+
+
+def getImageIds(repo :str, token :str) -> list[int]:
+    
+    try:
+        # Get all images
+        resp :requests.Response = requests.get(
+            url     = f"https://api.github.com/user/packages/container/{repo}/versions",
+            headers = {
+                "Authorization": f"Bearer {token}"
+            }
+        )
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)        
+
+    # Convert response to json
+    respJson :list[dict[str, Any]] = resp.json()
+
+    # Get all ids
+    ids :list[int] = []
+    for imageData in respJson:
+
+        # Get tags for image
+        tags :list[str] = imageData["metadata"]["container"]["tags"]
+        # If tag list is empty, add to id list
+        if not len(tags):
+            ids.append(imageData["id"])
+
+    return ids
+
+
+# Delete untagged images
+def deleteUntaggedImages(repo :str, token :str, ids :list[int]):
+    
+    for imageId in ids:
+        try:
+            requests.delete(
+                url     = f"https://api.github.com/user/packages/container/{repo}/versions/{imageId}",
+                headers = {
+                    "Authorization": f"Bearer {token}"
+                }
+            )
+        except requests.exceptions.RequestException as e:
+            print(f"Error while deleting image with id: {imageId}")
+
+    return
+
+
+if __name__ == '__main__':
+
+    # Get all images without tags
+    ids :list[int] = getImageIds(REPOSITORY, GITHUB_TOKEN)
+
+    # Print log
+    idsStrings :list[str] = [str(id) for id in ids]
+    print(f"Images with following ids will be deleted from repository '{REPOSITORY}': [{', '.join(idsStrings)}]")
+
+    # Delete all images from response above
+    deleteUntaggedImages(REPOSITORY, GITHUB_TOKEN, ids)
+
+    # Print log
+    print("Finished deleting ids, exiting program...")
+
+    exit(0)
